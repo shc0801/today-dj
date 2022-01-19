cc9e0be45b75d28163920cece601edc7a8abcab9
shc0801 <shc123987@naver.com>
2022-01-20 02:03:32 +0900

    feat: add MainRouter

--- src/router/MainRouter.tsx
+++ src/router/MainRouter.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from '../pages/MainPage';
 
 const MainRouter: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/" element={<MainPage />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
