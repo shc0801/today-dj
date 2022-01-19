7237ec29c700e951b524444ab491a627f282dd37
shc0801 <shc123987@naver.com>
2022-01-20 02:02:48 +0900

    feat: add import span, img

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Span, Img } from '../components/atomic'
 
 const Container = styled.div`
   width: 100%;
 `;
 
 const MainPage: React.FC = () => {
-  return <Container></Container>;
+  return <Container>asd</Container>;
 };
 
 export default MainPage;
